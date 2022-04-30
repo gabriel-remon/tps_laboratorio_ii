dd5566a9433644b45390366a04aa26476e9b0a79
gabriel-remon <remongabi16@gmail.com>
2022-04-30 16:44:26 -0300

    Entrega TP2

---
+++ Remon.Gabriel.2ºC.TP2/Entidades/Cliclomotor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+        #region "Constructores"
+
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color) 
+            : base(chasis, marca, color) { }
+
+        #endregion
+
+        #region "Metodos"
+
+        /// <summary>
+        /// Ciclomotor son 'Chico'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Chico;
+            }
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CICLOMOTOR\n");
+            sb.AppendLine((string)this);
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        #endregion
+    }
+}
